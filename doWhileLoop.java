3a479ff770d7fcb7428bff6fdfad75a68cebb2a1
mudit999 <500062539@stu.upes.ac.in>
2020-04-26 18:46:39 +0530

    Day2: Adding doWhile and forLoop

---
+++ doWhileLoop.java
@@ -0,0 +1,12 @@
+package CodeNext;
+
+public class doWhileLoop {
+	public static void main(String[] args) {
+		int counter = 11;
+		
+		do {
+			System.out.println(counter); // body of while loop
+			counter++;
+		}while(counter<=10);
+	}
+}
---
+++ forLoop.java
@@ -0,0 +1,11 @@
+package CodeNext;
+
+public class forLoop {
+	public static void main(String[] args) {
+		int total = 0;
+		for(int counter = 1; counter<=10; counter++) {
+			total +=counter; 
+		}
+		System.out.println(total);
+	}
+}
