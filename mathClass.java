15873e2dde91b3614b15062b9c14b529bfbf09d1
mudit999 <500062539@stu.upes.ac.in>
2020-04-26 18:48:02 +0530

    Adding useful some Math Library

---
+++ mathClass.java
@@ -0,0 +1,22 @@
+package CodeNext;
+
+public class mathClass {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-33)); // Absolute Value
+		
+		System.out.println(Math.ceil(6.2)); // Ceil
+		
+		System.out.println(Math.floor(6.5)); // floor
+
+		System.out.println(Math.max(8.5,7.9)); // max
+
+		System.out.println(Math.min(8.5,7.9)); // min
+		
+		System.out.println(Math.pow(5,5)); // power
+
+		System.out.println(Math.sqrt(9)); // square root
+
+	}
+
+}
