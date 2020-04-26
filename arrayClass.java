0b260f94fed5005d8f0ddf9e396b56a2048a6c10
mudit999 <500062539@stu.upes.ac.in>
2020-04-26 23:26:30 +0530

    Day2: Array Syntax

---
+++ arrayClass.java
@@ -0,0 +1,18 @@
+package CodeNext;
+
+public class arrayClass {
+	public static void main(String[] args) {
+		
+	// Array Syntax
+		int array[] = new int[10];
+		array[0] = 87;
+		//array[1] = 543;
+		//array[9] = 65;
+		  
+		  //Array Intializer
+	//	int array[] = {2,4,5,32,23,34};
+		  
+		System.out.println(array[0]);
+
+	}
+}
