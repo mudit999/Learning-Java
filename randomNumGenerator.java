00d08217ff871c1836cb2cbe71703a905774c4fd
mudit999 <500062539@stu.upes.ac.in>
2020-04-26 18:52:22 +0530

    Day2: Program of RandonNumber Generator using Random Library

--- randomNumGenerator.java
+++ randomNumGenerator.java
@@ -9,8 +9,7 @@ public class randomNumGenerator {
 		
 		for(int counter =1; counter<=10; counter++) {
 			number = 1+dice.nextInt(45);
-			//1+dice to get 1 to 6 rather than 0 to 6 because all computer counting starts from 0
-			
+			//1+dice to get 1 to 6 rather than 0 to 6 because all computer counting starts from 0			
 			System.out.print(number + " ");
 		}
 	}
