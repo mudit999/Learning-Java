8cac3d7e6bb68a461c81f0480d20f4e6b4dd67e8
mudit999 <500062539@stu.upes.ac.in>
2020-05-02 00:10:43 +0530

    Day6: Enumeration and EnumSet Concept

--- enumClass.java
+++ enumClass.java
@@ -1,13 +1,19 @@
 package CodeNext;
 import java.util.EnumSet;
+// import for range
 
 public class enumClass {
 
 	public static void main(String[] args) {
 		
-		// whenever enumeration is used, java automatically build array of constants and called enumerations is called 
+		// whenever enumeration is used, java automatically build array of constants and called enumerations is called - enumeration.values()
 		for(enumeration people: enumeration.values())
 			System.out.printf("%s\t%s\t%s\n", people,people.getDesc(), people.getYear());
 		
+		System.out.println("\n And now for the range of constants!!\n");
+		
+		for(enumeration people: EnumSet.range(enumeration.mudit, enumeration.Solar))
+			System.out.printf("%s\t%s\t%s\n", people,people.getDesc(), people.getYear());
+
 	}
 }
--- enumeration.java
+++ enumeration.java
@@ -1,12 +1,18 @@
 package CodeNext;
 
+
+// Main function is in enumClass.java file
 public enum enumeration {
 	//enumeration - used to declare constants
 	
 	// constants that are object!!
 	mudit("Nice", "22"),
 	none("fine","0"),
-	no("okay","0");
+	no("okay","0"),
+	blit("looks","1"),
+	Solar("sucks","1"),
+	RaiJada("Don't even have word","2");
+	
 	
 	private final String desc;
 	private final String year;
