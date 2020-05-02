f3472ce3a7ff056737e3e715c8ea7029164e20af
mudit999 <500062539@stu.upes.ac.in>
2020-05-03 00:35:17 +0530

    Day7: Static Keyword

---
+++ printStaticClass.java
@@ -0,0 +1,10 @@
+package CodeNext;
+
+public class printStaticClass {
+
+	public static void main(String[] args) {
+		staticClass member1 = new staticClass("Salman","Fox");
+		staticClass member2 = new staticClass("Khan", "Life");
+	}
+
+}
---
+++ staticClass.java
@@ -0,0 +1,18 @@
+package CodeNext;
+
+// Main class is in printStaticClass.java
+public class staticClass {
+	private String first;
+	private String last;
+	private static int members=0;
+	// this is common variable of first and last
+	// Salman can't see others(khan) but all can see the members count
+	
+	
+	public staticClass(String fn, String ln) {
+		first = fn;
+		last=ln;
+		members++;
+		System.out.printf("constructor for %s %s, members in the club: %d\n",first, last, members);
+	}
+}
